067182d7c5f16652cb38dc579f1f739f110256ac
twainy <wakasugi at gmail.com>
2013-05-12 20:05:26 +0900

    initial upload

---
+++ project/ScalaProjectBuild.scala
@@ -0,0 +1,17 @@
+import sbt._
+import sbt.Keys._
+
+object ScalaProjectBuild extends Build {
+
+  lazy val scalaProject = Project(
+    id = "scala-project",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "Scala Project",
+      organization := "org.example",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.9.2"
+      // add other settings here
+    )
+  )
+}
